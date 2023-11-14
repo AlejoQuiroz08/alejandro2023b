5c4b417a98c8bafce156b37d4d37e526a0b49c5a
Isaac-Leon1 <isaac.leon@epn.edu.ec>
2023-11-14 09:43:46 -0500

    Cambio

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("asdasdasd");
 		System.out.println("HOLAAAAA");
-		System.out.println("asdasd 123");
-
 
 	}
 }
