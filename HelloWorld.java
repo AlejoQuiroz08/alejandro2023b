7ca01843959d5ece3140e620e774eba9f45cff64
AlejoQuiroz08 <150806260+AlejoQuiroz08@users.noreply.github.com>
2023-11-14 09:11:43 -0500

    Primera versión

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("asdasdasd");
+		System.out.println("HOLAAAAA");
+		System.out.println("asdasd 123");
+
+
+	}
+}
